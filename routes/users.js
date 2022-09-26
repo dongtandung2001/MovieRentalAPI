d7fcebfcf27049cd6d7a877c16c3dba05b3bb9e7
Tan Dung Dong <dongtandung2001@gmail.com>
2022-09-25 22:07:19 -0700

    add register and Login endpoints

---
+++ routes/users.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const {User, validate} = require('../models/user');
+const _ = require('lodash');
+
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const {error} = validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({email: req.body.email});
+    if (user) return res.status(400).send('User already existed');
+
+
+    // encrypt password using 'bcrypt'
+    
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    await user.save();
+
+    // dont send password back to the respond
+    // use Lodash JS
+    // return new object with chosen properties
+
+    res.send(_.pick(user, ['_id','name', 'email']));
+
+    
+});
+
+
+module.exports = router;
