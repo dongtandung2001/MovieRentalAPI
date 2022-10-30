1c66696fea74b7faef8ecdb486452a86b424335c
Dung Dong <dongtandung2001@gmail.com>
2022-10-30 00:15:04 -0700

    Add options to connect db

--- startup/db.js
+++ startup/db.js
@@ -5,7 +5,11 @@ const config = require('config')
 module.exports = function () {
   const db = config.get('db');
   console.log('db', db);
-  mongoose.connect(db)
+  mongoose.connect(db, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
     .then(() => {
       console.log(`Connected to ${db}`)
     })
