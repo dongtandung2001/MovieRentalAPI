e59f90e34f980d11b6ec62885743bb8272feb459
Dung Dong <dongtandung2001@gmail.com>
2022-10-29 19:36:54 -0700

    debug

--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,7 @@ const config = require('config')
 
 module.exports = function () {
   const db = config.get('db');
-  console.log(db);
+  console.log('db', db);
   mongoose.connect(db)
     .then(() => {
       console.log(`Connected to ${db}`)
