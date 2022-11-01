adfce99552438757184c409c3a1b2f5f441645a4
Dung Dong <dongtandung2001@gmail.com>
2022-10-31 23:01:08 -0700

    Cors

--- index.js
+++ index.js
@@ -16,12 +16,25 @@ require("./startup/config")();
 // startup production
 require('./startup/prod')(app);
 
+// startup
+app.use(cors());
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-auth-token')
+    next()
+})
+
 // SHOW API ENDPOINT On /API
 app.get('/api', (req, res, next) => {
     res.status(200).json({ endpoint: "APIENDPOINT" });
 })
 
+
+
 console.log('index', process.env['vidly_db']);
+
 
 // throw new Error('Error');
 const port = process.env.PORT || 3000;
